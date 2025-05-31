a4baa8ddcfaef63ca2b73eb937fe0aec68949d97
brownQz <157855762+brownQz@users.noreply.github.com>
2025-05-31 13:53:05 +0700

    Update language.js

--- language/language.js
+++ language/language.js
@@ -3,11 +3,14 @@ import id from './id.js'
 import ru from './ru.js'
 import jp from './jp.js'
 import cn from './cn.js'
+import vi from './vi.js'
+
 
 export default {
   en,
   id,
   ru,
   jp,
-  cn
+  cn,
+  vi
 }
